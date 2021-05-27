bf4606391cf5c060a23072368678bc431e27e726
ajith-u <ajithfeb025@gmail.com>
2021-05-27 13:05:12 +0530

    Add ability to add a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,23 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
 
   #index send the each value to User class n user.rb
   #that return the each user details with patterns
   def index
     render plain: User.all.map { |user| user.to_pleasant_string }.join("\n")
+  end
+
+  #Add a new user
+  def create
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
+    new_user = User.create!(
+      name: name,
+      email: email,
+      password: password,
+    )
+    responce_text = "Hey your new user is created with the id #{new_user.id}"
+    render plain: responce_text
   end
 end
