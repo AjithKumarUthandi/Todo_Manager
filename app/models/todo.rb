dc0fd1d3a4abd6d0e5b0ce57d82499c1be742fb2
ajith-u <ajithfeb025@gmail.com>
2021-06-01 20:33:18 +0530

    Create a erb file named as index in views/todos

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,76 @@
 class Todo < ActiveRecord::Base
-  def to_pleasant_string
-    is_completed = completed ? "[X]" : "[ ]"
-    "#{id}. #{due_date.to_s(:short)} #{todo_text} #{is_completed}"
+  #find if the date is today
+  def due_today?
+    due_date == Date.today
+  end
+
+  #From Todo-list get each Todo and send to_displayable_string
+  def self.to_displayable_list
+    all.map { |todo| todo.to_displayable_string }
+  end
+
+  #display the value with id
+  #if completed, mentioned also the Todo
+  def to_displayable_string
+    display_status = completed ? "[X]" : "[ ]"
+    display_date = due_today? ? nil : due_date
+    "#{id}. #{display_status} #{todo_text} #{display_date}"
+  end
+
+  #found overdue todos
+  def self.overdue
+    where("due_date < ?", Date.today)
+  end
+
+  #found today due todos
+  def self.due_today
+    where("due_date = ?", Date.today)
+  end
+
+  #found which todos have a days
+  def self.due_later
+    where("due_date > ?", Date.today)
+  end
+
+  #Display the Todo-list with patterns
+  def self.show_list
+    date = Date.today
+    puts "My Todo-list\n\n"
+
+    puts "Overdue\n"
+    puts overdue.map { |todo| todo.to_displayable_string }
+    puts "\n\n"
+
+    puts "Due Today\n"
+    puts due_today.map { |todo| todo.to_displayable_string }
+    puts "\n\n"
+
+    puts "Due Later\n"
+    puts due_later.map { |todo| todo.to_displayable_string }
+    puts "\n\n"
+  end
+
+  #insert a new Todo
+  def self.add_task(h)
+    create!(todo_text: h[:todo_text], due_date: Date.today + h[:due_in_days], completed: false)
+  end
+
+  #update Todo is completed with help of id
+  def self.mark_as_complete(todo_id)
+    todo = find_by(id: todo_id)
+    if (todo) #there is id,then update
+      todo.completed = true
+      todo.save!
+      return todo
+    end
+    puts "Error! Invalid id" #else, print message and exit from here
+    exit
   end
 end
+
+# class Todo < ActiveRecord::Base
+#   def to_pleasant_string
+#     is_completed = completed ? "[X]" : "[ ]"
+#     "#{id}. #{due_date.to_s(:short)} #{todo_text} #{is_completed}"
+#   end
+# end
