ee8ca9091a06dd49e280cf7f99209bc24145c983
ajith-u <ajithfeb025@gmail.com>
2021-06-15 12:17:20 +0530

    email cannot repeat

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 #For User table
 class User < ActiveRecord::Base
   validates :first_name, presence: true
-  validates :email, presence: true
+  validates :email, {presence: true, uniqueness: true}
 
   has_secure_password
   has_many :todos
